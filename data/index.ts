436fd92db65eb013b4bb96536a31d897bc67c20f
Orewagenki <orewagenki3@gmail.com>
2025-01-25 12:33:52 -0700

    updated text in "inside scoop"

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
   {
     id: 5,
     title:
-      "Currently building a React Native App with Payments | PostgreSQL, TypeScript, Stripe, Tailwind",
+      "Currently building a React Native App with Payments using PostgreSQL, TypeScript, Stripe, Tailwind",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
