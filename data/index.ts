b91712ca7d1a3c9465d0dd0c6d93880d1efe39a6
Orewagenki <orewagenki3@gmail.com>
2025-02-13 11:45:05 -0700

    Updated projects and the insdie scoop details

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
   {
     id: 5,
     title:
-      "Currently building a React Native App with Payments using PostgreSQL, TypeScript, Stripe, Tailwind",
+      "Currently building an enterprise platform with Admin Dashboard | Next.js, PostgreSQL, Redis, Auth.js",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -105,11 +105,11 @@ export const projects = [
   },
   {
     id: 5,
-    title: "Portfolio Version 1",
-    des: "My first Attempt at creating a portfolio website",
-    img: "/workResume.png",
+    title: "Uber Clone",
+    des: "React Native App with Payments using PostgreSQL, TypeScript, Stripe, Tailwind",
+    img: "/uberC.png",
     iconLists: ["/ts.svg"],
-    link: "https://resume-website-six-flax.vercel.app/",
+    link: "https://github.com/Orewagenki/uber-clone",
   },
 ];
 
